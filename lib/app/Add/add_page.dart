6275d1452495425ea192ac2080be60b7cbe7532f
Patryk Laskowski <patrick_laskowski@hotmail.com>
2022-11-18 21:00:56 +0100

    add textfield and elevatedbutton in add page

--- lib/app/Add/add_page.dart
+++ lib/app/Add/add_page.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
   const AddPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  var productName = '';
+  var shopName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AddPage'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę produktu',
+            ),
+            onChanged: (newValue) {
+              setState(
+                () {
+                  productName = newValue;
+                },
+              );
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę/rodzaj sklepu',
+            ),
+            onChanged: (newValue) {
+              setState(
+                () {
+                  shopName = newValue;
+                },
+              );
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Dodaj'),
+          ),
+        ],
       ),
     );
   }
